e2fda17b44d7b1de966fb7c5b9658f102341d721
NNbaba <702331733@qq.com>
2019-03-05 14:49:53 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-
   res.render('index', { title: 'Express' });
 });
 
@@ -16,10 +14,38 @@ router.get('/first', function(req, res, next) {
 });
 
 
+router.get('/getauth', function(req, res, next) {
+	let id = req.query.id;
+	let str = "rtmp://yingxiutv.com";
+	let url = "/live/"+id;
+	const md5 = require("nodejs-md5");
+	let timt = Number.parseInt((new Date().getTime())/1000);
+	let time = timt + 1800;
+
+	md5.string(url+"-"+time+"-0-0-m4V7UdnmvZ",function(err,data){
+		let md5Str = data;
+		console.log("md5-----"+md5Str);
+		console.log("时间戳"+timt);
+		console.log("30分钟后时间戳"+time);
+		let arr = md5Str.split("=");
+
+		let sstr = str+url+"-"+time+"-0-0-"+arr[1].trim();
+
+		res.send({url:sstr});
+	});
+
+
+
+
+});
 
 router.get('/ali',function (req, res, next){
 	con = mysql.createConnection(config);
-	console.log(con);
+
+	console.log(req.query);
+	console.log(req.body);
+	con.end();
+
 })
 
 module.exports = router;
