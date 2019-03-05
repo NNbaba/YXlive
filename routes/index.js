71ec4988db8a97310a33f8f8200c149d83294a0c
NNbaba <702331733@qq.com>
2019-03-05 15:00:11 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -15,24 +15,24 @@ router.get('/first', function(req, res, next) {
 
 
 router.get('/getauth', function(req, res, next) {
-	// let id = req.query.id;
-	// let str = "rtmp://yingxiutv.com";
-	// let url = "/OAProjectLive/"+id;
-	// const md5 = require("nodejs-md5");
-	// let timt = Number.parseInt((new Date().getTime())/1000);
-	// let time = timt + 1800;
+	let id = req.query.id;
+	let str = "rtmp://yingxiutv.com";
+	let url = "/OAProjectLive/"+id;
+	const md5 = require("nodejs-md5");
+	let timt = Number.parseInt((new Date().getTime())/1000);
+	let time = timt + 1800;
 
-	// md5.string(url+"-"+time+"-0-0-m4V7UdnmvZ",function(err,data){
-	// 	let md5Str = data;
-	// 	console.log("md5-----"+md5Str);
-	// 	console.log("时间戳"+timt);
-	// 	console.log("30分钟后时间戳"+time);
-	// 	let arr = md5Str.split("=");
+	md5.string(url+"-"+time+"-0-0-m4V7UdnmvZ",function(err,data){
+		let md5Str = data;
+		console.log("md5-----"+md5Str);
+		console.log("时间戳"+timt);
+		console.log("30分钟后时间戳"+time);
+		let arr = md5Str.split("=");
 
-	// 	let sstr = str+url+"-"+time+"-0-0-"+arr[1].trim();
+		let sstr = str+url+"-"+time+"-0-0-"+arr[1].trim();
 
-	// 	res.send({url:sstr});
-	// });
+		res.send({url:sstr});
+	});
 
 
 
