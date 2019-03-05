52666c2a9fcbd176b08a4ca3c24c6560c3b3bf3b
NNbaba <702331733@qq.com>
2019-03-05 14:16:07 +0800

    端口更改

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var mysql = require("myql");
+var config = require("../min/connction");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	
 
-	
   res.render('index', { title: 'Express' });
 });
 
@@ -15,5 +15,11 @@ router.get('/first', function(req, res, next) {
   res.json({name:'aaa',pwd:'123'});
 });
 
+
+
+router.get('/ali',function (req, res, next){
+	con = mysql.createConnection(config);
+	console.log(con);
+})
 
 module.exports = router;
